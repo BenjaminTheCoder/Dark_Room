07c2b8c060ad94e8bb8e3e9c45d298d3c9342ad0
BenjaminTheCoder <161790365+BenjaminTheCoder@users.noreply.github.com>
2026-02-03 19:20:26 -0800

    Stae machine activated.

--- pymunk_test.py
+++ pymunk_test.py
@@ -6,6 +6,17 @@ from maze_generator_jotbleach import MazeGenerator #type: ignore
 from class_definitions import *
 from pprint import pprint
 import math
+from enum import Enum, auto
+
+class Screen(Enum):
+    TITLE = auto()
+    GAME = auto()
+    SETTINGS = auto()
+    PAUSE = auto()
+    WIN = auto()
+    LOSE = auto()
+
+
 
 def make_box(pos: Vec2d, space: pm.Space) -> pm.Body:
     poly_body = pm.Body(body_type=pm.Body.STATIC)
@@ -38,28 +49,37 @@ pr.init_window(WINDOWWIDTH, WINDOWHEIGHT, "Dark Room")
 
 pr.set_target_fps(FPS)
 
+pr.set_exit_key(pr.KeyboardKey.KEY_NULL)
+
+screen = Screen.TITLE
+play_button = 0
+settings_button = 0
 maze = make_maze()
 
 space = pm.Space()
 # space.gravity = Vec2d(0, -981)
 
 player_body = pm.Body()
-player_body.position = Vec2d(WINDOWWIDTH/2, -WINDOWHEIGHT/2 + 100)
+player_body.position = start_pos = Vec2d(round(CELL_WIDTH*(maze.start_pos[1] + 0.5)), -round(CELL_HEIGHT*(maze.start_pos[0] + 0.5)))
 
-player_shape = pm.Circle(player_body, 1)
+player_shape = pm.Circle(player_body, CELL_WIDTH/4)
 player_shape.mass = 10
 player_shape.elasticity = ELASTICITY
 space.add(player_body, player_shape)
 
 squares = build_maze(maze, space)
 
-
+end_trigger = make_box(Vec2d(round(CELL_WIDTH*(maze.end_pos[1] + 0.5)), -round(CELL_HEIGHT*(maze.end_pos[0] + 0.5))), space)
 
 print_options = pm.SpaceDebugDrawOptions()
 
-def input_handling(player_body: pm.Body, player_poly: pm.Circle) -> None:
+def input_handling(player_body: pm.Body, player_poly: pm.Circle,) -> None:
+    global screen
     if pr.is_key_down(pr.KeyboardKey.KEY_F):
         pr.toggle_fullscreen()
+    if pr.is_key_down(pr.KeyboardKey.KEY_ESCAPE) and screen == Screen.GAME:
+        screen = Screen.PAUSE
+        print("esc", screen)
     if pr.is_key_down(pr.KeyboardKey.KEY_W) or pr.is_key_down(pr.KeyboardKey.KEY_UP):
         player_body.velocity = Vec2d(player_body.velocity.x, player_body.velocity.y + SLIP)
     if pr.get_gamepad_axis_movement(0, pr.GamepadAxis.GAMEPAD_AXIS_LEFT_Y) > 0.1:
@@ -94,12 +114,48 @@ while not pr.window_should_close():
     space.step(1/FPS)
     # space.debug_draw(print_options)
 
+    # if player_body
+
+# button checks
+    if play_button == 1:
+        screen = Screen.GAME
+        play_button = 0
+
+    if settings_button == 1:
+        screen = Screen.SETTINGS
+        settings_button = 0
+
     pr.begin_drawing()
     pr.clear_background((144, 213, 255))
-    pr.draw_circle(round(player_body.position.x), round(-player_body.position.y), player_shape.radius, pr.BLUE)
-    for poly_box in squares:
-        pr.draw_rectangle(math.ceil(poly_box.position.x - CELL_WIDTH / 2), math.ceil(-poly_box.position.y - CELL_HEIGHT / 2), math.ceil(CELL_WIDTH), math.ceil(CELL_HEIGHT), pr.BLACK)
 
+    match screen:
+        case Screen.GAME:
+            pr.draw_circle(round(player_body.position.x), round(-player_body.position.y), player_shape.radius, pr.BLUE)
+            pr.draw_rectangle(math.ceil(end_trigger.position.x - CELL_WIDTH / 2), math.ceil(-end_trigger.position.y - CELL_HEIGHT / 2), math.ceil(CELL_WIDTH), math.ceil(CELL_HEIGHT), pr.GREEN)
+            for poly_box in squares:
+                pr.draw_rectangle(math.ceil(poly_box.position.x - CELL_WIDTH / 2), math.ceil(-poly_box.position.y - CELL_HEIGHT / 2), math.ceil(CELL_WIDTH), math.ceil(CELL_HEIGHT), pr.BLACK)
+            
+
+        case Screen.TITLE:
+            play_button = pr.gui_button(pr.Rectangle(WINDOWWIDTH/2-BUTTON_WIDTH/2, WINDOWHEIGHT/2-BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT), "PLAY")
+            settings_button = pr.gui_button(pr.Rectangle(WINDOWWIDTH/2-BUTTON_WIDTH/2, (WINDOWHEIGHT/2-BUTTON_HEIGHT/2+BUTTON_HEIGHT*1.5), BUTTON_WIDTH, BUTTON_HEIGHT), "SETTINGS")
+
+        case Screen.SETTINGS:
+            print("Settings")
+
+        case Screen.PAUSE:
+            print("Pause") 
+
+        case Screen.WIN:
+            pr.draw_rectangle(0, 0, WINDOWWIDTH, WINDOWHEIGHT, pr.BLACK)
+            pr.draw_text(f'You win!', 275, 270, 60, pr.WHITE)
+
+        case Screen.LOSE:
+            pr.draw_rectangle(0, 0, WINDOWWIDTH, WINDOWHEIGHT, pr.BLACK)
+            pr.draw_text(f'You lose!', 275, 270, 60, pr.WHITE)          
+
+        case _:
+            print("Chaos has conquered!!!")
     pr.end_drawing()
 pr.close_window()
 pprint(maze.__dict__)
